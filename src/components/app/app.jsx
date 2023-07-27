ee90a223b00eb93ff8e837be73405fd80d0f6219
Yakov Samsonov <yasamsonov@gmail.com>
2023-07-27 16:26:48 +0300

    Feat: render data from server

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,42 @@
 import AppHeader from "../app-header/app-header";
 import styles from "./app.module.css";
 import Order from "../order/order.jsx";
-import { data } from "../../utils/data.js";
+import { useState, useEffect } from "react";
+import { BACKEND_URL } from "../constants/constants";
 
 function App() {
+  const [state, setState] = useState({
+    isLoading: false,
+    hasError: false,
+    data: [],
+  });
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const loadData = () => {
+    setState({ ...state, hasError: false, isLoading: true });
+    fetch(BACKEND_URL)
+      .then((res) => res.json())
+      .then((d) => {
+        setState({ ...state, data: d.data, isLoading: false });
+      })
+      .catch((e) => {
+        setState({ ...state, hasError: true, isLoading: false });
+      });
+  };
+
   return (
     <div className={styles.app}>
       <AppHeader />
-      <Order data={data} />
+      {!state.isLoading && !state.hasError && <Order data={state.data} />}
+      {state.isLoading && !state.hasError && (
+        <p className={styles.warning}>Загрузка...</p>
+      )}
+      {!state.isLoading && state.hasError && (
+        <p className={styles.warning}>Что-то пошло не так...</p>
+      )}
     </div>
   );
 }
--- src/components/constants/constants.jsx
+++ src/components/constants/constants.jsx
@@ -4,3 +4,4 @@ export const MAIN_TYPE = "main";
 export const TOP_ING_TYPE = "top";
 export const BOTTOM_ING_TYPE = "bottom";
 export const REGULAR_ING_TYPE = "regular";
+export const BACKEND_URL = "https://norma.nomoreparties.space/api/ingredients";
