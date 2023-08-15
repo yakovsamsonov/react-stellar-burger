249b10bca9997ab734936ba868b430f18f7edc7a
Yakov Samsonov <yasamsonov@gmail.com>
2023-08-15 21:39:30 +0300

    Feat: setup redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
+import { rootReducer } from './services/reducers';
+import thunk from 'redux-thunk';
+
+const composeEnhancers =
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
